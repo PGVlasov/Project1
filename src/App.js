f49cd530427e2b09f42813a34172d55ca7ef4805
Oleg Korolko <olegk101@gmail.com>
2020-03-14 21:00:07 +0100

    Fix code styling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import ToDoList from './Todo/todolist';
-
+import React from "react";
+import ToDoList from "./Todo/todolist";
 
 function App() {
-    const [todos, setTodos] = React.useState([
-    {id:1, complided: false, title: "Купить продукт 1"},
-    {id:2, complided: false, title: "Купить продукт 2"},
-    {id:3, complided: false, title: "Купить продукт 3"},
-    {id:4, complided: false, title: "Купить продукт 4"},
-    {id:5, complided: true, title: "Купить продукт 5"} //при true  продукт должен быть зачернут, не раб
-  ])
+  const [todos, setTodos] = React.useState([
+    { id: 1, complided: false, title: "Купить продукт 1" },
+    { id: 2, complided: false, title: "Купить продукт 2" },
+    { id: 3, complided: false, title: "Купить продукт 3" },
+    { id: 4, complided: false, title: "Купить продукт 4" },
+    { id: 5, complided: true, title: "Купить продукт 5" } //при true  продукт должен быть зачернут, не раб
+  ]);
 
-  function selectMarkTodo (id) {
-      setTodos(
-        todos.map(todo => {
-            if(todo.id === id) {
-                todo.complided = !todo.complided 
-            }
+  function selectMarkTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.complided = !todo.complided;
+        }
 
-      return todo
-    })
-    )
+        return todo;
+      })
+    );
   }
 
   return (
     <div className="wrapper">
       <h1>Shopping list</h1>
-        <h2>Basket</h2>
-          <ToDoList todos = {todos} onSelectMark ={selectMarkTodo}/>
+      <h2>Basket</h2>
+      <ToDoList todos={todos} onSelectMark={selectMarkTodo} />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Todo/todoitems.js
+++ src/Todo/todoitems.js
@@ -1,61 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const styles = {
-    li: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '.5rem 1rem',
-        border: '1px solid #ccc',
-        borderRadius: '4px',
-        marginBottom: '.5rem'
-    },
-    input: {
-        marginRight: '1rem'
-    },
-    rem: {
-        background: 'green',
-        borderRadius: '4px',
-        color: '#fff',
-        border: 'none'
-    },
-    /*done: {
+  li: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginBottom: ".5rem"
+  },
+  input: {
+    marginRight: "1rem"
+  },
+  rem: {
+    background: "green",
+    borderRadius: "4px",
+    color: "#fff",
+    border: "none"
+  }
+  /*done: {
         textDecoration: 'line-through'
     }*/
-}
+};
 
-function ToDoItem ({todo, index, onChange}) {
-   /* console.log('todo', todo)*/ /*работает*/
-  const classes = []
+function ToDoItem({ todo, index, onChange }) {
+  /* console.log('todo', todo)*/ /*работает*/
+  const classes = [];
 
-    if (todo.complited) {
-        classes.push('done')/* должен зачеркнуться, не работает */
-    } 
+  if (todo.complited) {
+    classes.push("done"); /* должен зачеркнуться, не работает */
+  }
 
-
-    return (
-    <li style = {styles.li}>
-        <span className ={classes.join(' ')}>
-            <input 
-            type = "checkbox"
-            checked = {todo.complited} /* не работает*/
-            style={styles.input} 
-            onChange={() => onChange(todo.id)}
-            />
-        <strong>{ index + 1 }</strong>
+  return (
+    <li style={styles.li}>
+      <span className={classes.join(" ")}>
+        <input
+          type="checkbox"
+          checked={todo.complited} /* не работает*/
+          style={styles.input}
+          onChange={() => onChange(todo.id)}
+        />
+        <strong>{index + 1}</strong>
         {todo.title}
-        </span>
+      </span>
 
-        <button style ={styles.rem}>&times;</button>
+      <button style={styles.rem}>&times;</button>
     </li>
-    )
+  );
 }
 
 ToDoItem.propTypes = {
-    todo: PropTypes.object.isRequired,
-    index: PropTypes.number,
-    onChange: PropTypes.func.isRequired
-}
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    complited: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }).isRequired,
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
 
-export default ToDoItem
+export default ToDoItem;
